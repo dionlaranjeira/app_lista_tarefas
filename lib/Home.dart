8e8deec416f4135a2ccfa211b9a98f30e7be6840
DION RIBEIRO LARANJEIRA <dionribeiro.rr@gmail.com>
2022-01-21 09:54:21 -0400

    Interface do app

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,8 +8,58 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+
+  List<String> _tarefas = ["Agradecer a Deus", "Se alimentar bem", "Trabalhar", "Estudar", "Cuidar de mim"];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.purple,
+      ),
+      body: Column(
+        children: [
+          Expanded(child: ListView.builder(
+              itemCount: _tarefas.length,
+              itemBuilder: (context, index){
+                return ListTile(
+                  title: Text(_tarefas[index]),
+                );
+              }))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          showDialog(context: context,
+              builder: (context){
+                return AlertDialog(
+                 title: Text("Adicinar tarefa"),
+                 content: const TextField(
+                   decoration: InputDecoration(
+                     labelText: "Digite a tarefa",
+                   ),
+                   onChanged: null,
+                 ),
+                  actions: [TextButton(
+                      onPressed: (){
+                        Navigator.pop(context);
+                      },
+                      child: Text("Cancelar")),
+                    TextButton(
+                        onPressed: (){
+                          //salvar
+                          Navigator.pop(context);
+                        },
+                        child: Text("Salvar")),
+                  ],
+                );
+              });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.purple,
+      ),
+    );
   }
 }
